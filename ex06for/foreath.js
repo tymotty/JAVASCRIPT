76a3a87448633069dc6df810a5445a273527eb17
TDRN-097 <tdosreisnogueira@gmail.com>
2026-03-08 18:15:37 -0300

    ForEach js

---
+++ ex06for/foreath.js
@@ -0,0 +1,11 @@
+const users = [
+    {name: `Rodolefo`, age: 17, contact: `(19) 9341-3919`},
+    {name: `Timoteo`, age: 19, contact: `(19) 93346-5499`},
+    {name: `Sama`, age: 27, contact: `(19) 95431-3935`},
+    {name: `Geraldo`, age: 61, contact: `(19) 91345-3919`},
+]
+users.forEach(function(item, index){
+    if(item.age < 18){
+        console.log(`Olá ${item.name}, você não está permitido entrar nesta site por ser menor de idade!`)
+    }
+});
