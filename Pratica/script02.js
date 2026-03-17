11b7dfe197989c1f36838e538d793394a9226608
TDRN-097 <tdosreisnogueira@gmail.com>
2026-03-16 22:16:13 -0300

    pratica03

---
+++ Pratica/script02.js
@@ -0,0 +1,27 @@
+let listName = ['joao', 'pedro', 'matheus', 'paulo'];
+
+// o [] é a posicao no caso vai aparecer o joao
+
+listName[20] = 'judas';
+
+listName.push ('gae');
+
+listName.pop('matheus')
+
+let p = ('ped');
+
+let trazido = listName.push(p)
+
+
+
+console.log(listName)
+
+function filtrarPorF(nome){
+    if(name.stratsWith ("p")) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
