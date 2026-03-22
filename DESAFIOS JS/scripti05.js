cb994301282247338938d16a3c68c642bc4ba822
TDRN-097 <tdosreisnogueira@gmail.com>
2026-03-21 21:45:14 -0300

    desafio novo

--- DESAFIOS JS/scripti05.js
+++ DESAFIOS JS/scripti05.js
@@ -1,1 +1,7 @@
-let adicionarFeijao = document.querySelector ()
+const produto = document.querySelector(".product")
+const estoque = [
+    {name: 'Maça', valor: 5, quantidade: 2 },
+    {name: 'Feijao',valor: 8,quantidade:4 },
+    {name: 'Arroz',valor:15 ,quantidade:6 },
+    {name: 'Aveia',valor:12 ,quantidade:1 },
+]
