155a5dfd8a408cbf006c524f6dc03ea342d2edc5
TDRN-097 <tdosreisnogueira@gmail.com>
2026-03-30 22:49:13 -0300

    Desafio Carrinho: inicio js/estoque

---
+++ DESAFIOS JS/Mini Carrinho de Compras/script.js
@@ -0,0 +1,27 @@
+const carrinho = document.querySelector("#carrinho")
+const buttonProduto = document.querySelectorAll(".AdicionarProduto")
+
+
+
+const estoque = [
+  {
+    nome: "Mouse Gamer",
+    preco: 89.90,
+  },
+  {
+    nome: "Teclado Mecânico",
+    preco: 249.90,
+  },
+  {
+    nome: "Monitor 24 Polegadas",
+    preco: 899.90,
+  },
+  {
+    nome: "Headset Gamer",
+    preco: 179.90,
+  },
+  {
+    nome: "Mousepad Grande",
+    preco: 39.90,
+  }
+]
