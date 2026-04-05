a38618df9baf7b25b0cb4c9d7fca92d1e346453b
TDRN-097 <tdosreisnogueira@gmail.com>
2026-04-04 22:57:01 -0300

    ajuste de arquivo 2
    apenas coloque os Js nos dentro de um arquivo separado, ao inves de script

---
+++ Exercicios JS/EX01/script.js
@@ -0,0 +1,4 @@
+
+
+var nome = window.prompt("Qual seu nome?")
+alert("é um grande prazer te conhecer " + nome)
---
+++ Exercicios JS/EX02/script.js
@@ -0,0 +1,5 @@
+var n1 = Number.parseFloat(prompt("Digitre um numero: "))
+var n2 = Number.parseInt(prompt("Digite outro numero: "))
+var n3 = Number(prompt("Digite um terceiro numero: "))
+var soma = n1 + n2 + n3
+alert(`a soma entre ${n1}, ${n2} e ${n3} é igual a ${soma}`)
