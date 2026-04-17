b28456bb02164b47b97f3a185ab4c2ce52022514
TDRN-097 <tdosreisnogueira@gmail.com>
2026-04-16 22:48:57 -0300

    Switch JavaScript

---
+++ Exercicios JS/Ex09/Switch.js
@@ -0,0 +1,13 @@
+ let menu = 3
+
+ switch (menu){
+    case 1:
+        console.log("Login")
+        break
+    case 2: 
+        console.log("Cadastro")
+        break
+    case 3:
+        console.log ("Opcao invalida")
+        
+ }
