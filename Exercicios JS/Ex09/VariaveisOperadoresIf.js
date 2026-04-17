974767cc89205e3a66eb6316e4808fa7201130ab
TDRN-097 <tdosreisnogueira@gmail.com>
2026-04-16 22:49:14 -0300

    Variaveis Operadoes If else em JavaScript

---
+++ Exercicios JS/Ex09/VariaveisOperadoresIf.js
@@ -0,0 +1,21 @@
+//Crie um código que:
+//  Receba um número
+//  Verifique se é par ou ímpar
+
+let number = 8
+
+if (number % 2 == 0) {
+    console.log("Numero é Par")
+} else {
+    console.log("Numero é Impar")
+}
+
+//Verifique se uma pessoa pode dirigir (idade ≥ 18)
+const usuario =
+    {id: 1, nome: "Timóteo", idade: 20}
+
+if (usuario.idade >= 18) {
+    console.log ("Usuario Maior de idade")
+} else {
+    console.log("usuario menor de idade")
+}
