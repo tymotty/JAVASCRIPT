6155962b05b501b468fcf4d639f8044c7bf66769
TDRN-097 <tdosreisnogueira@gmail.com>
2026-04-17 22:25:39 -0300

    inicio de Desafio Lista

---
+++ DESAFIOS JS/Mini Desafio/script.js
@@ -0,0 +1,11 @@
+const botao = document.querySelector("#Botao")
+const input = document.querySelector("#input")
+const lista = document.querySelector("#Lista")
+
+botao.addEventListener("click", adicionar)
+
+
+
+function adicionar() {
+
+}
