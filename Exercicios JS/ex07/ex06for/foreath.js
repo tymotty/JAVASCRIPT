e6612eaed79c7415c0a3fc8ff1e56fd93c8daae9
TDRN-097 <tdosreisnogueira@gmail.com>
2026-04-19 22:51:05 -0300

    ajuste

--- Exercicios JS/ex07/ex06for/foreath.js
+++ Exercicios JS/ex07/ex06for/foreath.js
@@ -4,8 +4,17 @@ const users = [
     {name: `Sama`, age: 27, contact: `(19) 95431-3935`},
     {name: `Geraldo`, age: 61, contact: `(19) 91345-3919`},
 ]
+
+//IDADE MINIMA = 18
+const MINIMUM_AGE = 18
+
 users.forEach(function(item, index){
-    if(item.age < 18){
+
+    if(item.age < MINIMUM_AGE){
         console.log(`Olá ${item.name}, você não está permitido entrar nesta site por ser menor de idade!`)
+    } 
+    
+    else {
+        console.log (`Olá ${item.name}, você está permitido entrar nesta site por ser maior de idade! Esperamos que tenha uma ótima experiencia com nosso sistema, qualquer reclacamacao entre em contato com nossos desenvolvedores`)
     }
 });
