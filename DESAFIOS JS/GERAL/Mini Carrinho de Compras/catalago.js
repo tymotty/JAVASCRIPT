da52b757617f8f03e2e7ee4e13c67c6abf6169f8
TDRN-097 <tdosreisnogueira@gmail.com>
2026-07-05 20:31:54 -0300

    Catalago

---
+++ DESAFIOS JS/GERAL/Mini Carrinho de Compras/catalago.js
@@ -0,0 +1,6 @@
+//Animacao do botao de ver catalagos
+document.querySelectorAll(".categoria button").forEach(botao => {
+    botao.addEventListener("click", () => {
+        alert("Em breve novos produtos nesta categoria");
+    });
+});
